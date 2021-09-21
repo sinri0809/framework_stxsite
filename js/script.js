6467149421bf94bc90f964996ebcf8d2c3150f39
sinri0809 <dev.1lliodqb@gmail.com>
2021-09-21 15:37:15 +0900

    stxconst_ver.2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
-$(document).ready(function(){	
-	$('.gnb').mouseenter(function(){
-		$('.header').addClass('header_open');
+$(document).ready(function () {
+	const header = $('.header');
+	const header_gnb = header.find('.gnb');
+	const header_open = 'header_open';
+	
+	header_gnb.hover(function () {
+		header.toggleClass(header_open);
 	});
-
-	$('.gnb').mouseleave(function(){		
-		$('.header').removeClass('header_open');
-	});
-
-	// 사업파트로 이동하기
-	$('.move-cate').click(function(e){
-		// 클릭된 정보를 차단한다.
-		// href 를 막는다.
-		e.preventDefault();
-		var tgY = $('.cate').offset().top;
-		$('html, body').animate({
-			scrollTop: tgY
-		});
-
-	});
-
-	// 안내창 닫기
+	// modal
 	$('.modal').click(function(){
 		$('.modal').fadeOut();
 	});
-
 });
 
